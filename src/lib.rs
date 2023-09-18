d8f1f60c4d745b40ba5fe941ecbc13603e280376
CLog <88345999+CLOG9@users.noreply.github.com>
2023-09-18 21:55:21 +0200

    pub

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,19 @@
+pub mod database;
+pub use database::*;
+pub mod encrypt;
+pub use encrypt::*;
+pub mod profile {
+    pub(crate) mod history;
+    pub(crate) mod user;
+}
+pub mod password {
+    pub(crate) mod generator;
+    pub(crate) mod manager;
+}
+pub use password::*;
+pub use profile::*;
 pub mod sayhi {
+    pub use super::*;
     pub fn hi() {
         println!("hey");
     }
