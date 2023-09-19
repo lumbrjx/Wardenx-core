b9b51e1872de773be4ea8bac53dbc9ad124eb060
CLog <88345999+CLOG9@users.noreply.github.com>
2023-09-20 00:32:19 +0200

    get history function

--- src/profile/history.rs
+++ src/profile/history.rs
@@ -5,6 +5,8 @@ use cuid2::cuid;
 pub use crate::database::*;
 use chrono::Local;
 pub mod history {
+    use rusqlite::params;
+
     use super::*;
     use crate::database::warden_db::db_connect;
     // create new history record
@@ -22,5 +24,26 @@ pub mod history {
         let conn = db_connect()?;
         conn.execute("DELETE FROM history", [])?;
         Ok(())
+    }
+    pub struct Record {
+        pub record: String,
+        pub date_time: String,
+    }
+    pub fn get_history() -> Result<Vec<Record>> {
+        let conn = db_connect()?;
+        let mut stmt = conn.prepare("SELECT * FROM history")?;
+        let rows = stmt.query_map(params![], |row| {
+            Ok(Record {
+                record: row.get(1)?,
+                date_time: row.get(2)?,
+            })
+        })?;
+
+        let mut records = Vec::new();
+        for record in rows {
+            records.push(record?);
+        }
+
+        Ok(records)
     }
 }
