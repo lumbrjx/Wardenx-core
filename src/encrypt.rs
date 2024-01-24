9b05f64c0183853fd2ce3c85b9588717d2d40d07
CLog <88345999+CLOG9@users.noreply.github.com>
2024-01-24 06:54:48 +0100

    df1t

--- src/encrypt.rs
+++ src/encrypt.rs
@@ -1,29 +1,24 @@
+use df1t_cyph::{df1t_decrypt, df1t_encrypt};
 use digest::Digest;
 use sha2::Sha256;
 
-use aes_gcm::{
-    aead::{Aead, AeadCore, KeyInit, OsRng},
-    Aes256Gcm,
-};
 #[derive(PartialEq)]
 pub enum EncType {
     ENC,
     DEC,
 }
-pub fn encrypt_pass(pass: String, enc_t: EncType) -> Result<Vec<u8>, aes_gcm::Error> {
-    let key = Aes256Gcm::generate_key(OsRng);
-    let cipher = Aes256Gcm::new(&key);
-
-    let nonce = Aes256Gcm::generate_nonce(&mut OsRng); // 96-bits; unique per message
-    if enc_t == EncType::ENC {
-        let ciphertext = cipher.encrypt(&nonce, pass.as_ref())?;
-        println!("{:?}", ciphertext);
-        return Ok(ciphertext);
-    } else {
-        let plaintext = cipher.decrypt(&nonce, pass.as_ref())?;
-        println!("{:?}", plaintext);
-
-        return Ok(plaintext);
+pub fn encrypt_pass(pass: String, salt: String) -> String {
+    let res = df1t_encrypt(pass, salt);
+    match res {
+        Ok(t) => t,
+        Err(e) => panic!(),
+    }
+}
+pub fn decrypt_pass(pass: String, salt: String) -> String {
+    let res = df1t_decrypt(pass, salt);
+    match res {
+        Ok(t) => t,
+        Err(e) => panic!(),
     }
 }
 
@@ -44,45 +39,4 @@ pub fn hash_string(input: &str) -> String {
         .collect::<String>();
 
     hash_hex
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_encrypt_and_decrypt() {
-        // Arrange
-        let password = "secret_password".to_string();
-
-        // Act
-        let encrypted_result = encrypt_pass(password.clone(), EncType::ENC);
-        assert!(encrypted_result.is_ok());
-        let encrypted_password = encrypted_result.unwrap();
-        if let Ok(s) = String::from_utf8(encrypted_password) {
-            let decrypted_result = encrypt_pass(s, EncType::DEC);
-            assert!(decrypted_result.is_ok());
-            let decrypted_password = decrypted_result.unwrap();
-            assert_eq!(password.as_bytes(), decrypted_password.as_slice());
-        } else {
-            println!("Invalid UTF-8 sequence");
-        }
-
-        // Assert
-    }
-
-    #[test]
-    fn test_invalid_decryption() {
-        // Arrange
-        let invalid_password = vec![1, 2, 3]; // Invalid ciphertext
-        if let Ok(s) = String::from_utf8(invalid_password) {
-            let result = encrypt_pass(s, EncType::DEC);
-
-            // Assert
-            assert!(result.is_err());
-        } else {
-            println!("Invalid UTF-8 sequence");
-        }
-        // Act
-    }
 }
